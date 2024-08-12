bff4ba29f6706615263a82d442594763ba873cee
Pedro Carmo <pedro.m.a.carmo@gmail.com>
2024-08-12 20:11:56 +0100

    Adding coverage to exceptions

--- tests/TransporterTest.php
+++ tests/TransporterTest.php
@@ -160,7 +160,125 @@ final class TransporterTest extends TestCase
         }
     }
 
-    public function testTransporterWithClientWithNoJsonException(): void
+    public function testTransporterWithWrongJsonWithErrorException(): void
+    {
+        {
+            $transporter = new Transporter(
+                $clientMock = self::createMock(ClientInterface::class),
+                $baseUri = BaseUri::from('http://example.com'),
+                $headers = Headers::create(),
+                $queryParams = QueryParams::create()->withParam('access_token', 'access-token-1')
+            );
+
+            $payload = Payload::create(
+                'POST',
+                'threads',
+                ['hello' => 'world'],
+                ['this_is_a_body' => 'with_content'],
+                ['content/type' => 'json']
+            );
+
+            $response = self::createMock(ResponseInterface::class);
+
+            $streamInterface = self::createMock(StreamInterface::class);
+            $streamInterface->expects($this->once())
+                ->method('getContents')
+                ->willReturn('this_is_not_an_json');
+
+            $response
+                ->expects(self::once())
+                ->method('getHeaderLine')
+                ->with('Content-Type')
+                ->willReturn('application/json');
+
+            $response
+                ->expects(self::once())
+                ->method('getStatusCode')
+                ->willReturn(500);
+
+            $response
+                ->expects(self::once())
+                ->method('getBody')
+                ->willReturn($streamInterface);
+
+            $clientMock
+                ->expects(self::once())
+                ->method('sendRequest')
+                ->with(self::callback(function ($request) use ($baseUri, $headers, $queryParams, $payload) {
+                    return $request->getMethod() === $payload->method()
+                        && (string) $request->getUri() === (string) $payload->toRequest($baseUri, $headers, $queryParams)->getUri()
+                        && $request->getHeaders() == $payload->toRequest($baseUri, $headers, $queryParams)->getHeaders();
+                }))
+                ->willReturn($response);
+
+            self::expectException(UnserializableResponse::class);
+            $transporter->request($payload);
+        }
+    }
+
+    public function testTransporterWithClientWithStatusCode500Exception(): void
+    {
+        $transporter = new Transporter(
+            $clientMock = self::createMock(ClientInterface::class),
+            $baseUri = BaseUri::from('http://example.com'),
+            $headers = Headers::create(),
+            $queryParams = QueryParams::create()->withParam('access_token', 'access-token-1')
+        );
+
+        $payload = Payload::create(
+            'POST',
+            'threads',
+            ['hello' => 'world'],
+            ['this_is_a_body' => 'with_content'],
+            ['content/type' => 'json']
+        );
+
+        $response = self::createMock(ResponseInterface::class);
+        $clientException = self::createMock(ClientException::class);
+
+        $streamInterface = self::createMock(StreamInterface::class);
+        $streamInterface->expects($this->once())
+            ->method('getContents')
+            ->willReturn(
+                json_encode(['error' => ['message' => 'Error', 'code' => 500]])
+            );
+
+        $response
+            ->expects(self::once())
+            ->method('getBody')
+            ->willReturn($streamInterface);
+
+        $response
+            ->expects(self::once())
+            ->method('getStatusCode')
+            ->willReturn(500);
+
+        $response
+            ->expects(self::once())
+            ->method('getHeaderLine')
+            ->with('Content-Type')
+            ->willReturn('application/json');
+
+        $clientException
+            ->expects(self::exactly(2))
+            ->method('getResponse')
+            ->willReturn($response);
+
+        $clientMock
+            ->expects(self::once())
+            ->method('sendRequest')
+            ->with(self::callback(function ($request) use ($baseUri, $headers, $queryParams, $payload) {
+                return $request->getMethod() === $payload->method()
+                    && (string) $request->getUri() === (string) $payload->toRequest($baseUri, $headers, $queryParams)->getUri()
+                    && $request->getHeaders() == $payload->toRequest($baseUri, $headers, $queryParams)->getHeaders();
+            }))
+            ->willThrowException($clientException);
+
+        self::expectException(ErrorException::class);
+        $transporter->request($payload);
+    }
+
+    public function testTransporterWithClientWithNoJsonTypeException(): void
     {
         $transporter = new Transporter(
             $clientMock = self::createMock(ClientInterface::class),
@@ -199,7 +317,7 @@ final class TransporterTest extends TestCase
             ->expects(self::once())
             ->method('getHeaderLine')
             ->with('Content-Type')
-            ->willReturn('application/json');
+            ->willReturn('none');
 
         $clientException
             ->expects(self::exactly(2))
@@ -216,8 +334,7 @@ final class TransporterTest extends TestCase
             }))
             ->willThrowException($clientException);
 
-        self::expectException(ErrorException::class);
+        self::expectException(TransporterException::class);
         $transporter->request($payload);
     }
-
 }
